7bd993f6a56ef4c8890f7ffb5c15ad5f78d89e87
Chandrashu Kumar <kchandrashu@gmail.com>
2025-03-17 15:15:00 +0530

    authentication done yes

--- core/views.py
+++ core/views.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
+    if request.user.is_authenticated:  # If user is logged in
+        return redirect('/')
     if(request.method == 'POST'):
         username = request.POST['username']
         password = request.POST['password']
@@ -25,13 +29,44 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, password=password, email=email)
 
+                user_login = auth.authenticate(username=username, password = password)
+                auth.login(request, user_login)
 
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
 
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Passwords do not match')
             return redirect('signup')
     else: 
         return render(request, 'signup.html')
+
+def signin(request):
+    if request.user.is_authenticated:  # If user is logged in
+        return redirect('/')
+
+    if(request.method == 'POST'):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+    
+@login_required(login_url='signin')
+def signout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'settings.html')
